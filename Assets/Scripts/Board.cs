d4607029a59bb8088b8b91fead4ec026f6b4e078
noahssarcastic <niniguezg@gmail.com>
2021-08-22 22:57:57 -0500

    Much reorganize

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,8 +8,7 @@ public class Board : MonoBehaviour {
 
     [SerializeField] private int width = 8;
     [SerializeField] private int height = 8;
-
-    [SerializeField] private Sprite tileSprite;
+    [SerializeField] private GameObject tilePrefab;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,27 +17,7 @@ public class Board : MonoBehaviour {
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                // Create tile
-                GameObject tile = new GameObject(string.Format("Tile {0}, {1}", i, j), typeof(SpriteRenderer));
-                tile.transform.parent = gameObject.transform;
-                tile.transform.localPosition = new Vector3(i, j);
-
-                // Render tile
-                SpriteRenderer sr = tile.GetComponent<SpriteRenderer>();
-                sr.sprite = tileSprite;
-                if ((i+j) % 2 == 0) {
-                    sr.color = new Color(87/256f, 58/256f, 46/256f);
-                } else {
-                    sr.color = new Color(252/256f, 204/256f, 116/256f);
-                }
-
-                // Create debug textmesh
-                TextMesh tm = WorldText.CreateWorldText(
-                    tileMap.GetTile(i, j).ToString(), 
-                    tileMap.GetWorldCoords(i, j) + gameObject.transform.position, 
-                    Color.white);
-                tm.transform.parent = tile.transform;
-
+                GameObject tile = CreateTile();
                 tiles[i, j] = tile;
             }
         }
@@ -46,22 +25,44 @@ public class Board : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector3 offset = gameObject.transform.position;
-            Vector3 clickCoords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            (int, int) clickedTile = tileMap.GetGridCoords(clickCoords);
-            tileMap.SetTile(clickedTile.Item1, clickedTile.Item2, 1);
-        }
+        // if (Input.GetMouseButtonDown(0)) {
+        //     Vector3 offset = gameObject.transform.position;
+        //     Vector3 clickCoords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //     (int, int) clickedTile = tileMap.GetGridCoords(clickCoords);
+        //     tileMap.SetTile(clickedTile.Item1, clickedTile.Item2, 1);
+        // }
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                GameObject tile = tiles[i, j];
-                tile.GetComponentInChildren<TextMesh>().text = tileMap.GetTile(i, j).ToString();
-            }
-        }
+        // for (int i = 0; i < width; i++) {
+        //     for (int j = 0; j < height; j++) {
+        //         GameObject tile = tiles[i, j];
+        //         tile.GetComponentInChildren<TextMesh>().text = tileMap.GetTile(i, j).ToString();
+        //     }
+        // }
     }
 
-    private void CreateTile() {
+    private GameObject CreateTile() {
+        // Create tile
+        GameObject tile = Instantiate(tilePrefab);
 
+        // GameObject tile = new GameObject(string.Format("Tile {0}, {1}", i, j), typeof(SpriteRenderer));
+        // tile.transform.parent = gameObject.transform;
+        // tile.transform.localPosition = new Vector3(i, j);
+
+        // // Render tile
+        // SpriteRenderer sr = tile.GetComponent<SpriteRenderer>();
+        // // sr.sprite = tileSprite;
+        // if ((i+j) % 2 == 0) {
+        //     sr.color = new Color(87/256f, 58/256f, 46/256f);
+        // } else {
+        //     sr.color = new Color(252/256f, 204/256f, 116/256f);
+        // }
+
+        // // Create debug textmesh
+        // TextMesh tm = WorldText.CreateWorldText(
+        //     tileMap.GetTile(i, j).ToString(), 
+        //     tileMap.GetWorldCoords(i, j) + gameObject.transform.position, 
+        //     Color.white);
+        // tm.transform.parent = tile.transform;
+        return tile;
     }
 }
